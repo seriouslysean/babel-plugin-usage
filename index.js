a27fb48657f0e47b2b42c1c36c55d0820a72ee20
Sean Kennedy <skennedy@urbn.com>
2020-01-22 11:34:43 -0500

    More comments explaining the features

--- index.js
+++ index.js
@@ -8,22 +8,30 @@ function logFeature(title, message, ...args) {
 }
 
 // Arrow Functions
+// Compact alternative to a normal function expression, maintains this context
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 const addValues = (a, b) => a + b;
 logFeature('Arrow Functions', 'addValues', addValues(1, 2));
 
-// Destructuring
+// Destructuring Assignment
+// Plucks values from arrays or properties from objects in to a variable
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 const numberArr = [1, 2, 3];
 const [one,,three] = numberArr;
 logFeature('Destructuring', 'numberArr, one, three', numberArr, one, three);
 
-// Shorthand Properties
+// Shorthand Property Names
+// Add a new property to an object with a single reference, the variable name becoming the key
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Syntax
 const a = 'A';
 const b = 'B';
 const c = 'C';
 const letterObj = { a, b, c };
 logFeature('Shorthand Properties', 'a, b, c, letterObj', a, b, c, letterObj);
 
-// Spread Syntax
+// Spread Syntax / Sprwad Operator
+// Allows creating copies of object with new or updated values, functions similarly to Object.assign(object1, object2)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 const letterArr1 = ['A'];
 const letterArr2 = ['B', 'C'];
 const letterArr3 = [
@@ -33,12 +41,17 @@ const letterArr3 = [
 logFeature('Spread Syntax', 'letterArr1, letterArr2, letterArr3', letterArr1, letterArr2, letterArr3);
 
 // Nullish Colescing Operator
+// A logical operator that returns the right side of the operation if the left is undefined or null. Similar in usage to OR ||
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
 const zeroName = 0 ?? 'Anonymous (Zero)';
 const falseName = false ?? 'Anonymous (False)';
 const undefinedName = undefined ?? 'Anonymous (Undefined)';
 logFeature('Nullish Colescing Operator', 'zeroName, falseName, undefinedName', zeroName, falseName, undefinedName);
 
 // Optional Chaining Operator
+// Allows reading the value of a property deep in an object without validating every reference
+// Short circuits and returns undefined if an optional chain is missing
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 const animals = {
     cat: {
         name: 'Mr. Whiskers',
@@ -57,5 +70,7 @@ const animals = {
 const catLegs = animals.cat.legs;
 const dogLegs = animals.dog?.legs;
 const horseFood = animals.horse.favoriteFood?.[0];
+// Not something we should do, use invoke instead
+// This will return undefined but we don't know if it's from the actual function call or the lack of a function
 const getTotalLegs = animals.methods.getTotalLegs?.();
 logFeature('Optional Chaining Operator', 'catLegs, dogLegs, horseFood, getTotalLegs', catLegs, dogLegs, horseFood, getTotalLegs);
